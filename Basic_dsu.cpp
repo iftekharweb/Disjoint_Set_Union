d139b2524579f591b2633f6b8d05f6707fa2ae98
Iftekhar Md. Shishir <84434133+iftekharweb@users.noreply.github.com>
2022-04-25 02:29:41 +0600

    Update Basic_dsu.cpp

--- Basic_dsu.cpp
+++ Basic_dsu.cpp
@@ -2,6 +2,7 @@ Problems:
     https://www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/city-and-campers/
     https://www.hackerearth.com/problem/algorithm/city-and-flood-1/
     https://codeforces.com/problemset/problem/277/A
+    https://codeforces.com/problemset/problem/1559/D1
 
 1.  Time Complexity : O(a(n))
     -> Path Compression
